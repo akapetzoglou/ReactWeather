e01ff042a90059908c819b5a5db582c6cf955d27
teakap <tasos.kapetzoglou@gmail.com>
2017-01-16 21:45:57 +0200

    Added css custom file

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,9 @@ var Weather = require('Weather');
 var About = require('About');
 var Examples =require('Examples');
 
+// App css
+require('style!css!applicationStyles')
+
 // Load Foundation
 require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
